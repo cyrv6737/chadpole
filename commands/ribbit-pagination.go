0aa86b8785b1fa13d0b5ddbe7d1bf6d6ebfeb4eb
cyrv6737 <cyrv6737@protonmail.com>
2023-08-15 05:07:47 -0400

    Add some example data and circular pagination

--- commands/ribbit-pagination.go
+++ commands/ribbit-pagination.go
@@ -15,10 +15,6 @@ KNOWN ISSUES:
     This will ensure that different handlers are assigned to the pagination every time.
     The drawback of this is that there are now essentially "dead handlers" attached to the bot. Maybe
     the garbage collector deals with it at some point. The fuck do I know. At least it works now
-
-TODO:
-  - Add circular pagination and max pages
-  - Look into using the response message ID as the prefix instead of generating my own random string
 */
 package commands
 
@@ -44,6 +40,13 @@ type PaginationView struct {
 	embeddesc       string
 	handerPrefix    string
 	pageBtnHandlers map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate)
+	exampleData     []FrogExample // Create a slice of the struct where our data will be stored.
+	currentPage     int
+}
+
+type FrogExample struct {
+	frogName string
+	frogDesc string
 }
 
 /*
@@ -62,6 +65,7 @@ func (p *PaginationView) Setup(s *discordgo.Session, i *discordgo.InteractionCre
 	p.pageBtnHandlers[p.handerPrefix+"ts_last"] = p.TSLastBtnHandler
 	// Add the handlers to the bot
 	p.TSAddHandlers(s, i)
+	p.currentPage = p.index + 1
 }
 
 /*
@@ -89,13 +93,17 @@ func (p *PaginationView) GenPrefix() string {
 Creates the embed. This function is called every time there is an update to the message
 */
 func (p *PaginationView) CreateEmbed() []*discordgo.MessageEmbed {
-	p.embedtitle = fmt.Sprintf("%d", p.index)
-	p.embeddesc = fmt.Sprintf("%d", p.index)
+	// Pull data from Data slice based on the index which is modified with the buttons
+	p.embedtitle = fmt.Sprintf("%s", p.exampleData[p.index].frogName)
+	p.embeddesc = fmt.Sprintf("%s", p.exampleData[p.index].frogDesc)
 
 	embed := []*discordgo.MessageEmbed{
 		{
 			Title:       p.embedtitle,
 			Description: p.embeddesc,
+			Footer: &discordgo.MessageEmbedFooter{
+				Text: fmt.Sprintf("Page: %d/%d", p.currentPage, len(p.exampleData)),
+			},
 		},
 	}
 
@@ -157,7 +165,8 @@ Importantly, there is a check to make sure that there are no running paginations
 Handers are added several times because currently they are set to add once to avoid duplication
 */
 func (p *PaginationView) SendMessage(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	p.Setup(s, i) // Call setup function first so the handler prefix can be generated and all handlers added
+	p.Setup(s, i)               // Call setup function first so the handler prefix can be generated and all handlers added
+	p.currentPage = p.index + 1 // Display page number normally
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
@@ -175,6 +184,7 @@ func (p *PaginationView) SendMessage(s *discordgo.Session, i *discordgo.Interact
 Updates the message every time next or prev is pressed
 */
 func (p *PaginationView) UpdateMessage(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	p.currentPage = p.index + 1 // Display page number normally
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseUpdateMessage,
 		Data: &discordgo.InteractionResponseData{
@@ -195,7 +205,7 @@ a data structure
 func (p *PaginationView) TSNextBtnHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	p.Lock()
 	defer p.Unlock()
-	p.index++
+	p.index = (p.index + 1) % len(p.exampleData) // Circular pagination
 	log.Printf("[INFO] Pagination %s data incremented", p.handerPrefix)
 	p.UpdateMessage(s, i)
 }
@@ -203,18 +213,25 @@ func (p *PaginationView) TSNextBtnHandler(s *discordgo.Session, i *discordgo.Int
 func (p *PaginationView) TSPrevBtnHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	p.Lock()
 	defer p.Unlock()
-	p.index--
-	log.Printf("[INFO] Pagination %s data decremented", p.handerPrefix)
-	p.UpdateMessage(s, i)
+	if p.index == 0 { // Prevent running out of bounds. Function as a "last" button if index is at 0
+		p.index = len(p.exampleData) - 1
+		p.UpdateMessage(s, i)
+	} else {
+		p.index = (p.index - 1) % len(p.exampleData) // Circular pagination
+		log.Printf("[INFO] Pagination %s data decremented", p.handerPrefix)
+		p.UpdateMessage(s, i)
+	}
 }
 
 func (p *PaginationView) TSFirstBtnHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	log.Printf("[INFO] Pagination %s data set to first", p.handerPrefix)
+	p.index = 0
 	p.UpdateMessage(s, i)
 }
 
 func (p *PaginationView) TSLastBtnHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	log.Printf("[INFO] Pagination %s data set to last index", p.handerPrefix)
+	p.index = len(p.exampleData) - 1
 	p.UpdateMessage(s, i)
 }
 
@@ -277,6 +294,13 @@ func RibbitPaginationHandler(s *discordgo.Session, i *discordgo.InteractionCreat
 	new_pagination := PaginationView{
 		index:           0,
 		pageBtnHandlers: make(map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate)), // Must create the map for the handler CustomIDs
+		exampleData: []FrogExample{ // Declare our data here outside of the struct
+			{"Red-eyed Tree Frog", "Known for its vibrant green coloration, the red-eyed tree frog is native to the rainforests of Central and South America. It has striking red eyes and orange feet, and it spends most of its life in trees near water bodies."},
+			{"Poison Dart Frog", "Poison dart frogs are small and brightly colored frogs found in Central and South America. They are known for their toxic skin secretions, which have been used by indigenous people to poison the tips of blowgun darts for hunting."},
+			{"African Clawed Frog", "Native to sub-Saharan Africa, the African clawed frog is an aquatic species that lacks tongue and teeth. Its distinctive \"claws\" on its hind feet are used for digging and defense."},
+			{"Goliath Frog", "The Goliath frog is the largest frog species, found in Cameroon and Equatorial Guinea in Africa. It can grow to be over a foot long and has a unique appearance with robust body proportions."},
+			{"Wood Frog", "Wood frogs are found in North America and are known for their remarkable adaptation to cold environments. They can survive freezing temperatures by entering a state of suspended animation and then thawing back to life when temperatures rise."},
+		},
 	}
 	log.Println("[INFO] New pagination created")
 	new_pagination.SendMessage(s, i) // Send the message, functions as the entrypoint for the pagination view
